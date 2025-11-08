f8ae6ea501275b27b61e483b439f27100d96f2e9
cyberseekerx <kanekarayush777@proton.me>
2025-11-08 11:45:10 +0530

    problem 1 finshed

--- problem_set6/lines/lines.py
+++ problem_set6/lines/lines.py
@@ -1,13 +1,25 @@
 import sys 
 
+def main():
+    try:
+        if len(sys.argv) > 2  :
+            sys.exit("too many command-line arguments")
+        if not sys.argv[1].endswith('.py') :
+            sys.exit("Not a Python file")
+        count_lines(sys.argv[1])
 
-alphabates = tuple("abcdefghijklmnopqrstuvwxyz")
-
-i = 0
-with open(sys.argv[1],"r") as s:
-    for lines in s :
-        if lines.lstrip().lower().startswith(alphabates):
-            i += 1
-
-print(i)
-"""hello"""
+    except IndexError:
+        sys.exit("Too few command-line  arguments")
+def count_lines(f):
+    try:
+        alphabates = tuple('abcdefghijklmnopqrstuvwxyz"""')
+        i = 0
+        with open(f,"r") as s:
+            for lines in s :
+                if not (lines.lstrip().startswith("#") or lines.strip() == ""):
+                    i += 1
+            print(i)
+    except FileNotFoundError:
+        sys.exit("File not found")
+if __name__ == "__main__":
+    main()
