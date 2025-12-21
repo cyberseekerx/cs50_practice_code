da04e972813694dd76f86640b58624b230a9165b
cyberseekerx <kanekarayush777@proton.me>
2025-12-21 14:52:43 +0530

    practice with pillow

--- problem_set6/shirt/shirt.py
+++ problem_set6/shirt/shirt.py
@@ -3,6 +3,9 @@ from PIL import Image
 
 def main():
     cli_input()
+    edit_image()
+
+
 
 
 def cli_input():
@@ -11,14 +14,25 @@ def cli_input():
     elif len(sys.argv) < 3:
         sys.exit(f'too few command-line arguments {len(sys.argv)}')
     elif sys.argv[1].endswith(('.jpg','.png','.jpeg')) and sys.argv[2].endswith(('.jpg','.png','.jpeg')):
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
         if (sys.argv[1].endswith('.jpg') and sys.argv[2].endswith('.jpg')):
-            print('jpg')
+            return input_file , output_file
         elif (sys.argv[1].endswith('.png') and sys.argv[2].endswith('.png')):
-            print('.png')
+            return input_file , output_file
         elif (sys.argv[1].endswith('.jpeg') and sys.argv[2].endswith('.jpeg')):
-            print('.jpeg')
+            return input_file ,output_file
         else:
             sys.exit("input and output have different extentions")
+
+
+def edit_image():
+    shirt = Image.open('shirt.png')
+    size = shirt.size
+    photo.paste(shirt,shirt)
+    print(shirt)
+    print(size)
+
 
 
 
