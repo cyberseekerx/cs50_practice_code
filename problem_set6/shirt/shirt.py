2722dfe8d61fb302fc91f041e93a6993e47c304a
cyberseekerx <kanekarayush777@proton.me>
2025-12-23 11:10:11 +0530

    finished shirt

--- problem_set6/shirt/shirt.py
+++ problem_set6/shirt/shirt.py
@@ -1,30 +1,48 @@
 import sys 
-from PIL import Image
+from PIL import Image, ImageOps
 
-def main():
-    cli_input()
-
-
-def cli_input():
-    if len(sys.argv) > 3:
-        sys.exit(f'too many command-line arguments {len(sys.argv)}')
-    elif len(sys.argv) < 3:
-        sys.exit(f'too few command-line arguments {len(sys.argv)}')
-    elif sys.argv[1].endswith(('.jpg','.png','.jpeg')) and sys.argv[2].endswith(('.jpg','.png','.jpeg')):
-        if (sys.argv[1].endswith('.jpg') and sys.argv[2].endswith('.jpg')):
-            print('jpg')
-        elif (sys.argv[1].endswith('.png') and sys.argv[2].endswith('.png')):
-            print('.png')
-        elif (sys.argv[1].endswith('.jpeg') and sys.argv[2].endswith('.jpeg')):
-            print('.jpeg')
-        else:
-            sys.exit("input and output have different extentions")
+##only two input accepted 
 
 
+if len(sys.argv) > 3:
+    sys.exit('Too many command-line arguments')
+elif len(sys.argv) < 3:
+    sys.exit('Too few command-line arguments')
+
+input = sys.argv[1]
+output = sys.argv[2]
+
+try:
+    file_input,input_extention = input.split('.')
+
+    file_output,output_extention = output.split('.')
+except:
+   sys.exit('invalid input')
+#check if the file are .jpg ,png ,jpeg 
+
+
+extentions = input_extention == output_extention
+if  input_extention and output_extention not in ('jpg','png','jpeg') :
+    sys.exit('invalid input')
+if not extentions :
+    sys.exit('Input and output have different extensions')
+try:
+    #input size  1200 x 1600
+    input = Image.open(input)
+    shirt = Image.open('shirt.png')
+    size = shirt.size
+    input = ImageOps.fit(input,size) 
+    ##crop and resize the shirt.png
+    shirt = ImageOps.fit(shirt,size,method=0 ,bleed =(0.0),centering = (0.5,0.5))
+    input.paste(shirt,(0,0),mask = shirt)
+    input.save(output)
+    
+
+
+    
+except FileNotFoundError:
+    sys.exit('Input does not exit')
 
 
 
-
-
-if __name__ == '__main__':
-    main()
+    
